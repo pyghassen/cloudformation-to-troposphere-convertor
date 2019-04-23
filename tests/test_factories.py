7af09485d993cbed15958eec4e9455ba1f2d60e2
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-24 00:22:45 +0200

    Adding tests for the factories.

---
+++ tests/test_factories.py
@@ -0,0 +1,30 @@
+from troposphere import Template, Ref, Output
+
+from app.factories import create_template
+
+
+def test_create_template():
+    description = "Test description"
+    metadata = {
+        "Metadata": {
+            "DependsOn": [],
+            "Environment": "Development",
+            "StackName": "Development-VPC"
+        }
+    }
+    internet_gateway_output = Output("InternetGateway", Value=Ref("InternetGateway"))
+    vpc_id_output = Output("VPCID", Value=Ref("VPC"))
+
+    outputs = [internet_gateway_output, vpc_id_output]
+
+    expected_output = {
+        "InternetGateway": internet_gateway_output,
+        "VPCID": vpc_id_output
+    }
+
+    template = create_template(description, metadata, outputs)
+
+    assert isinstance(template, Template)
+    assert template.description == description
+    assert template.metadata == metadata
+    assert template.outputs == expected_output
