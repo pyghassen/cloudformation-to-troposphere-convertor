00425ca8cafd337281afdb7b2e7874cbc8e98f01
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-25 08:33:35 +0200

    Adding create_resource factory function.

--- app/factories.py
+++ app/factories.py
@@ -2,14 +2,24 @@
 Factories module contains all the factories functions to create troposphere
 template.
 """
-from troposphere import Template
+from troposphere import Template, ec2
+from app.helpers import get_resource_type_name, get_properties
 
 
-def create_template(description, metadata, outputs):
+def create_template(description, metadata, outputs, resources):
     """Creates troposphere template."""
     template = Template()
     template.set_description(description)
     template.set_metadata(metadata)
     template.add_output(outputs)
-
+    for resource in resources:
+        template.add_resource(resource)
     return template
+
+
+def create_resource(resource_title, resource_values):
+    """Creates troposphere resource."""
+    resource_type_name = get_resource_type_name(resource_values)
+    resource_class = getattr(ec2, resource_type_name)
+    properties = get_properties(resource_values)
+    return resource_class(resource_title, **properties)
