8b5cd4b2ce554cc8584f01e9b88d544f621c84c0
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-25 19:23:20 +0200

    Adapting the test helpers to the conftest data new value.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -49,27 +49,86 @@ def test_get_resources(data):
                 ]
             },
             "Type": "AWS::EC2::InternetGateway"
+        },
+        "VPC": {
+            "Properties": {
+                "CidrBlock": "10.0.0.0/16",
+                "EnableDnsHostnames": "true",
+                "EnableDnsSupport": "true",
+                "InstanceTenancy": "default",
+                "Tags": [
+                    {
+                        "Key": "Environment",
+                        "Value": "Development"
+                    },
+                    {
+                        "Key": "Name",
+                        "Value": "Development-ServiceVPC"
+                    }
+                ]
             },
-            "VPC": {
-                "Properties": {
-                    "CidrBlock": "10.0.0.0/16",
-                    "EnableDnsHostnames": "true",
-                    "EnableDnsSupport": "true",
-                    "InstanceTenancy": "default",
-                    "Tags": [
-                        {
-                            "Key": "Environment",
-                            "Value": "Development"
-                        },
-                        {
-                            "Key": "Name",
-                            "Value": "Development-ServiceVPC"
-                        }
-                    ]
+            "Type": "AWS::EC2::VPC"
+        },
+        "VpcGatewayAttachment": {
+            "Properties": {
+                "InternetGatewayId": {
+                    "Ref": "InternetGateway"
                 },
-                "Type": "AWS::EC2::VPC"
-            }
+                "VpcId": {
+                    "Ref": "VPC"
+                }
+            },
+            "Type": "AWS::EC2::VPCGatewayAttachment"
+        },
+        "VpcNetworkAcl": {
+            "Properties": {
+                "Tags": [
+                    {
+                        "Key": "Environment",
+                        "Value": "Development"
+                    },
+                    {
+                        "Key": "Name",
+                        "Value": "Development-NetworkAcl"
+                    }
+                ],
+                "VpcId": {
+                    "Ref": "VPC"
+                }
+            },
+            "Type": "AWS::EC2::NetworkAcl"
+        },
+        "VpcNetworkAclInboundRule": {
+            "Properties": {
+                "CidrBlock": "0.0.0.0/0",
+                "Egress": "false",
+                "NetworkAclId": {
+                    "Ref": "VpcNetworkAcl"
+                },
+                "PortRange": {
+                    "From": "443",
+                    "To": "443"
+                },
+                "Protocol": "6",
+                "RuleAction": "allow",
+                "RuleNumber": 100
+            },
+            "Type": "AWS::EC2::NetworkAclEntry"
+        },
+        "VpcNetworkAclOutboundRule": {
+            "Properties": {
+                "CidrBlock": "0.0.0.0/0",
+                "Egress": "true",
+                "NetworkAclId": {
+                    "Ref": "VpcNetworkAcl"
+                },
+                "Protocol": "6",
+                "RuleAction": "allow",
+                "RuleNumber": 200
+            },
+            "Type": "AWS::EC2::NetworkAclEntry"
         }
+    }
 
     resources = get_resources(data)
     assert resources == expected_data
@@ -82,6 +141,8 @@ def test_get_resources(data):
         ({"Type": "AWS::EC2::VPC"}, "VPC"),
         ({"Type": "AWS::EC2::VPCGatewayAttachment"}, "VPCGatewayAttachment"),
         ({"Type": "AWS::EC2::NetworkAcl"}, "NetworkAcl"),
+        ({"Type": "AWS::EC2::NetworkAclEntry"}, "NetworkAclEntry"),
+        # ({"Type": "AWS::EC2::NetworkAcl"}, "NetworkAcl"),
     ]
 )
 def test_get_resource_type_name(resource_values, expected_type_name):
